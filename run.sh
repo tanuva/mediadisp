21980d454b66a630eb202a9eef1cf35e47b44fee
Marcel Brüggebors <marcel@nightsoul.org>
2015-02-26 18:35:24 +0100

    let run.sh cope with home-built libraries

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python src/xbmcdisp.py
+# On Debian, serdisplib needs to be built with libusb support for usb displays.
+# Need to modify LD_LIBRARY_PATH for it to be found.
+LD_LIBRARY_PATH=/usr/local/lib python src/xbmcdisp.py
