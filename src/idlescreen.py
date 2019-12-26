a4c1d5770de650fee9db7b215d58cf1d7a342c45
Marcel Kummer <tanuva@posteo.de>
2019-12-26 10:49:00 +0100

    Reindent consistently
    That's what you get when you finally configure vim properly on the less used computer.

--- src/idlescreen.py
+++ src/idlescreen.py
@@ -4,55 +4,55 @@ import requests
 import widget as gd
 
 class ICalPoller:
-	def __init__(self, url):
-		self.url = url
-		self.lastCalendarPoll = datetime(1970, 1, 1)
-		self.iCalText = ""
+    def __init__(self, url):
+        self.url = url
+        self.lastCalendarPoll = datetime(1970, 1, 1)
+        self.iCalText = ""
 
-	def getCalendarText(self):
-		if (datetime.now() - self.lastCalendarPoll).days < 1:
-			return self.iCalText
+    def getCalendarText(self):
+        if (datetime.now() - self.lastCalendarPoll).days < 1:
+            return self.iCalText
 
-		try:
-                    response = requests.get(self.url)
-		    if not response.status_code == 200:
-			    print("Error fetching calendar. HTTP status:", response.status_code)
-			    return "Nope."
+        try:
+            response = requests.get(self.url)
+            if not response.status_code == 200:
+                print("Error fetching calendar. HTTP status:", response.status_code)
+                return "Nope."
 
-                    self.iCalText = response.text
-		    self.lastCalendarPoll = datetime.now()
-		    return self.iCalText
-                except:
-                    print("Calendar GET request failed:", self.url)
-                    return "Nope."
+            self.iCalText = response.text
+            self.lastCalendarPoll = datetime.now()
+            return self.iCalText
+        except:
+            print("Calendar GET request failed:", self.url)
+            return "Nope."
 
-	def getEvent(self, title):
-		cal = Calendar.from_ical(self.getCalendarText())
-		countdownEvent = None
-		for component in cal.subcomponents:
-			if "SUMMARY" in component.keys() and component["SUMMARY"] == title:
-				countdownEvent = component
-		return countdownEvent
+    def getEvent(self, title):
+        cal = Calendar.from_ical(self.getCalendarText())
+        countdownEvent = None
+        for component in cal.subcomponents:
+            if "SUMMARY" in component.keys() and component["SUMMARY"] == title:
+                countdownEvent = component
+        return countdownEvent
 
 class IdleScreen:
-	def __init__(self, disp, settings):
-		self.disp = disp
-		self.settings = settings
-		self.poller = ICalPoller(self.settings.countdown["ical"])
-		self.bg = gd.Pixmap(disp, "layout-idle.png", [0, 0])
-		self.time = gd.Text(disp, [0,6], "DroidSans.ttf", 50, "00:00", halign = "center", valign = "top")
-		self.days = gd.Text(disp, [0,3], "DroidSans.ttf", 12, "000 Tage", halign = "center", valign = "bottom")
+    def __init__(self, disp, settings):
+        self.disp = disp
+        self.settings = settings
+        self.poller = ICalPoller(self.settings.countdown["ical"])
+        self.bg = gd.Pixmap(disp, "layout-idle.png", [0, 0])
+        self.time = gd.Text(disp, [0,6], "DroidSans.ttf", 50, "00:00", halign = "center", valign = "top")
+        self.days = gd.Text(disp, [0,3], "DroidSans.ttf", 12, "000 Tage", halign = "center", valign = "bottom")
 
-	def daysLeft(self):
-		event = self.poller.getEvent(self.settings.countdown["eventTitle"])
-		interval = event['DTSTART'].dt - datetime.now().date()
-		return interval.days
+    def daysLeft(self):
+        event = self.poller.getEvent(self.settings.countdown["eventTitle"])
+        interval = event['DTSTART'].dt - datetime.now().date()
+        return interval.days
 
-	def update(self):
-		self.bg.draw()
-		curTime = datetime.now()
-		self.time.setText("{:%H:%M}".format(curTime))
-		self.time.draw()
-		if self.settings.countdown["enabled"]:
-			self.days.setText("{0} Tage".format(self.daysLeft()))
-			self.days.draw()
+    def update(self):
+        self.bg.draw()
+        curTime = datetime.now()
+        self.time.setText("{:%H:%M}".format(curTime))
+        self.time.draw()
+        if self.settings.countdown["enabled"]:
+            self.days.setText("{0} Tage".format(self.daysLeft()))
+            self.days.draw()
