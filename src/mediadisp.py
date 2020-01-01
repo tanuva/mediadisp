984ba38184ea5e4152ae30010c9f48f66f837880
Marcel Kummer <tanuva@posteo.de>
2020-01-01 21:05:54 +0100

    Switch indentation to 4 spaces

--- src/mediadisp.py
+++ src/mediadisp.py
@@ -9,12 +9,12 @@ import os, sys, inspect
 # realpath() with make your script run, even if you symlink it :)
 cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
 if cmd_folder not in sys.path:
-	sys.path.insert(0, cmd_folder)
+    sys.path.insert(0, cmd_folder)
 
 # use this if you want to include modules from a subfolder
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"../pyserdisp/PySerdisp")))
 if cmd_subfolder not in sys.path:
-	sys.path.insert(0, cmd_subfolder)
+    sys.path.insert(0, cmd_subfolder)
 # ======================
 
 from time import sleep
@@ -25,57 +25,57 @@ from plexscreen import MusicScreen
 from idlescreen import IdleScreen
 
 class MediaDisp:
-	def __init__(self, serdisp):
-		self.__serdisp = serdisp
-		self.__wasDisplayOn = True
-		self.__screens = {}
-		self.__screens["music"] = MusicScreen(self.__serdisp, Settings)
-		self.__screens["idle"] = IdleScreen(self.__serdisp, Settings)
+    def __init__(self, serdisp):
+        self.__serdisp = serdisp
+        self.__wasDisplayOn = True
+        self.__screens = {}
+        self.__screens["music"] = MusicScreen(self.__serdisp, Settings)
+        self.__screens["idle"] = IdleScreen(self.__serdisp, Settings)
 
-	def __s(self, name):
-		return self.__screens[name]
+    def __s(self, name):
+        return self.__screens[name]
 
-	def run(self):
-		# isDisplayOn needs the currently active players
-		players = self.__s("music").getPlayers()
-		isDisplayOn = self.isDisplayOn(players)
+    def run(self):
+        # isDisplayOn needs the currently active players
+        players = self.__s("music").getPlayers()
+        isDisplayOn = self.isDisplayOn(players)
 
-		if self.__wasDisplayOn and not isDisplayOn:
-			self.__serdisp.quit()
-			self.__wasDisplayOn = False
-		elif not self.__wasDisplayOn and isDisplayOn:
-			self.__serdisp.init()
-			self.__serdisp.clear()
-			self.__wasDisplayOn = True
+        if self.__wasDisplayOn and not isDisplayOn:
+            self.__serdisp.quit()
+            self.__wasDisplayOn = False
+        elif not self.__wasDisplayOn and isDisplayOn:
+            self.__serdisp.init()
+            self.__serdisp.clear()
+            self.__wasDisplayOn = True
 
-		if not isDisplayOn:
-			return
+        if not isDisplayOn:
+            return
 
-		if "audio" in players:
-			self.__s("music").update()
-		else:
-			# Display something useful :)
-			self.__s("idle").update()
+        if "audio" in players:
+            self.__s("music").update()
+        else:
+            # Display something useful :)
+            self.__s("idle").update()
 
-		self.__serdisp.update()
+        self.__serdisp.update()
 
-	def isDisplayOn(self, players):
-		curTime = datetime.now()
-		isOn = False
+    def isDisplayOn(self, players):
+        curTime = datetime.now()
+        isOn = False
 
-		# rule priority increasing downward (obviously)
-		if curTime.hour >= 7 and curTime.hour < 23:
-			isOn = True
-		if "audio" in players:
-			isOn = True
-		if "video" in players:
-			isOn = False
+        # rule priority increasing downward (obviously)
+        if curTime.hour >= 7 and curTime.hour < 23:
+            isOn = True
+        if "audio" in players:
+            isOn = True
+        if "video" in players:
+            isOn = False
 
-		return isOn
+        return isOn
 
 if __name__ == "__main__":
-	with Serdisp(Settings.dispDevice, Settings.dispModel) as serdisp:
-		disp = MediaDisp(serdisp)
-		while True:
-			disp.run()
-			sleep(10)
+    with Serdisp(Settings.dispDevice, Settings.dispModel) as serdisp:
+        disp = MediaDisp(serdisp)
+        while True:
+            disp.run()
+            sleep(10)
--- src/settings.default.py
+++ src/settings.default.py
@@ -1,34 +1,34 @@
 class Settings:
-	"""
-	This is the actual configuration file. It is interpreted like ordinary Python code,
-	so you can do any kind of magic here that comes to your mind.
-	"""
+    """
+    This is the actual configuration file. It is interpreted like ordinary Python code,
+    so you can do any kind of magic here that comes to your mind.
+    """
 
-	# URL of your XBMC instance's web interface.
-	#xbmcHost = "http://raspberry.pi:80"
-	xbmcHost = "http://localhost:8080"
+    # URL of your XBMC instance's web interface.
+    #xbmcHost = "http://raspberry.pi:80"
+    xbmcHost = "http://localhost:8080"
 
-	plex = {
-		"user": "your@myplex.user",
-		"password": "your_myplex_password",
-		"servername": "human-readable Plex server name"
-	}
+    plex = {
+        "user": "your@myplex.user",
+        "password": "your_myplex_password",
+        "servername": "human-readable Plex server name"
+    }
 
-	# Display device as Serdisplib expects it
-	# See http://serdisplib.sourceforge.net/docs/index.html#serdisp_connect__SDCONN_open
-	#dispDevice = "USB:7c0/1501"
-	dispDevice = "USB:7c0/1501"
+    # Display device as Serdisplib expects it
+    # See http://serdisplib.sourceforge.net/docs/index.html#serdisp_connect__SDCONN_open
+    #dispDevice = "USB:7c0/1501"
+    dispDevice = "USB:7c0/1501"
 
-	# Display model as Serdisplib expects it
-	# Look for your display on http://serdisplib.sourceforge.net/#displays, where
-	# "Name in serdisplib" is the value that goes here.
-	#dispModel = "CTINCLUD"
-	dispModel = "CTINCLUD"
+    # Display model as Serdisplib expects it
+    # Look for your display on http://serdisplib.sourceforge.net/#displays, where
+    # "Name in serdisplib" is the value that goes here.
+    #dispModel = "CTINCLUD"
+    dispModel = "CTINCLUD"
 
-	countdown = {
-		"enabled": False,
-		# Title of the event in the calendar that is the countdown target
-		"eventTitle": "Countdown Event",
-		# URL to the iCal calendar file (polled every 24 hrs)
-		"ical": "https://url/to/ical"
-	}
+    countdown = {
+        "enabled": False,
+        # Title of the event in the calendar that is the countdown target
+        "eventTitle": "Countdown Event",
+        # URL to the iCal calendar file (polled every 24 hrs)
+        "ical": "https://url/to/ical"
+    }
