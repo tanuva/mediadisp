6733c6f9f93c7347f64a0ec5c3950ec75577b771
Marcel Kummer <tanuva@posteo.de>
2020-03-07 15:24:57 +0100

    Reorganize music screen
    Less lines, bigger content. No song album and progress bar anymore,
    but clock, title and artist are much more legible across the room now.

--- src/musicscreen.py
+++ src/musicscreen.py
@@ -139,12 +139,9 @@ class MusicScreen:
         self.bg = gd.Pixmap(disp, "layout-music.png", [0, 0])
         # text slots
         font = "DroidSans.ttf"
-        self.time   = gd.Text(disp, [0,2], font, 12, "00:00", halign = "center")
-        self.title  = gd.Text(disp, [0,14], font, 14, "Title", halign = "center")
-        self.artist = gd.Text(disp, [3,35], font, 12, "Artist")
-        self.album  = gd.Text(disp, [3,50], font, 12, "Album")
-        # progress bar
-        self.progress = gd.Progressbar(disp, [0,30], [128,4], border=False)
+        self.title  = gd.Text(disp, [0,0], font, 18, "PpJjÄäPpJjÄä Title")
+        self.artist = gd.Text(disp, [0,24], font, 16, "PpJjÄä Artist")
+        self.time   = gd.Text(disp, [0,51], font, 18, "00:00", halign = "right")
 
         self.dataProviders = [
             ShairportDataProvider(settings),
@@ -180,7 +177,3 @@ class MusicScreen:
         self.title.draw()
         self.artist.setText(tags["artist"])
         self.artist.draw()
-        self.album.setText(tags["album"])
-        self.album.draw()
-        self.progress.setState(progress)
-        self.progress.draw()
